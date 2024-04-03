ebf9f97648ae2343cef487325fd4631200dce87d
Tahn04 <112209139+Tahn04@users.noreply.github.com>
2024-04-03 14:34:36 -0600

    Added database

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -24,6 +24,26 @@ const hbs = handlebars.create({
     partialsDir: __dirname + '/views/partials',
   });
 
+const dbConfig = {
+  host: "db", // the database server
+  port: 5432, // the database port
+  database: process.env.POSTGRES_DB, // the database name
+  user: process.env.POSTGRES_USER, // the user account to connect with
+  password: process.env.POSTGRES_PASSWORD, // the password of the user account
+};
+
+const db = pgp(dbConfig);
+
+// test your database
+db.connect()
+  .then((obj) => {
+    console.log("Database connection successful"); // you can view this message in the docker compose logs
+    obj.done(); // success, release the connection;
+  })
+  .catch((error) => {
+    console.log("ERROR:", error.message || error);
+  });
+
 // Register `hbs` as  view engine using its bound `engine()` function.
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
