9ed7838598cb73a9ca9c0c244a7bf437cfa1c0a5
Prachi Soni <prso4895@colorado.edu>
2024-04-10 14:35:34 -0600

    previous recommendations query

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -3,3 +3,13 @@ CREATE TABLE users(
     username VARCHAR(50) PRIMARY KEY,
     password CHAR(60) NOT NULL
 );
+
+DROP TABLE IF EXISTS recommendations;
+CREATE TABLE recommendations(
+    id INT PRIMARY KEY,  -- Unique identifier
+    track_name VARCHAR(255) NOT NULL,
+    artist_name VARCHAR(255) NOT NULL,
+    album_image_url VARCHAR(1000) NOT NULL,
+    recommended_for INT,  -- Foreign key referencing user ID
+    FOREIGN KEY (recommended_for) REFERENCES users(id) -- Link recommendations to users
+);
