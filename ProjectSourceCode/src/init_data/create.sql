5f16443fe0d45b33cf57122bc6ad484c844a5d22
Maggie <mamu5358@colorado.edu>
2024-04-17 16:15:43 -0600

    tables made

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -13,3 +13,32 @@ CREATE TABLE recommendations(
     recommended_for INT,  -- Foreign key referencing user ID
     FOREIGN KEY (recommended_for) REFERENCES users(id) -- Link recommendations to users
 );
+
+DROP TABLE IF EXISTS moods;
+CREATE TABLE moods(
+    -- Let's see how to connect
+    mid INT PRIMARY KEY,
+    mood VARCHAR(255) NOT NULL,
+    -- related_genre VARCHAR(255) NOT NULL,
+    -- FOREIGN KEY (related_genre) REFRENCES genres()
+);
+
+DROP TABLE IF EXISTS genres;
+CREATE TABLE genres(
+    gid INT PRIMARY KEY,
+    genre VARCHAR(255) NOT NULL,
+    -- related_moods VARCHAR(255) NOT NULL,
+    -- FOREIGN KEY (related_genre) REFRENCES genres()
+);
+
+-- connection -2foeigh keys, -one is genre id, one is mood id
+--left merge connect to moods, then left merge w/genres
+DROP TABLE IF EXISTS connect;
+CREATE TABLE connect(
+    -- id INT PRIMARY KEY,
+    -- related_genre VARCHAR(255) NOT NULL,
+    FOREIGN KEY (gid) REFRENCES genres()
+    FOREIGN KEY (mid) REFRENCES moods()
+);
+
+
---
+++ ProjectSourceCode/src/init_data/insert.sql
@@ -0,0 +1,38 @@
+INSERT INTO moods (mid, mood)
+VALUES(1, 'happy'), (2, 'calm'), (3, 'sad'), (4, 'angry'), (5,'party'), 
+(6,'laugh'),(7,'rock')
+
+INSERT INTO genres (gid, genre)
+VALUES(1, 'acoustic'), (2, 'afrobeat'), (3, 'alt-rock'), (4, 'alternative'),
+(5, 'ambient' ), (6, 'anime'), (7, 'black-metal' ), (8, 'bluegrass'), 
+(9, 'blues' ), (10, 'bossanova' ), (11, 'brazil'), (12, 'breakbeat' ), 
+(13, 'british' ), (14, 'cantopop'), (15, 'chicago-house'), (16, 'children'), 
+(17, 'chill'), (18, 'classical' ), (19, 'club'), (20, 'comedy'), 
+(21, 'country'), (22, 'dance'), (23, 'dancehall' ), (24, 'death-metal'),
+(25, 'deep-house' ), (26, 'detroit-techno'), (27, 'disco' ), (28, 'disney'), 
+(29, 'drum-and-bass'), (30, 'dub' ), (31, 'dubstep'), (32, 'edm'), 
+(33, 'electro'), (34, 'electronic'), (35, 'emo'), (36, 'folk'), 
+(37, 'forro'), (38, 'french'), (39, 'funk'), (40, 'garage'), (41, 'german'),
+(42, 'gospel'), (43, 'goth'), (44, 'grindcore'), (45, 'groove'), 
+(46, 'grunge'), (47, 'guitar'), (48, 'happy'), (49, 'hard-rock'), 
+(50, 'hardcore'), (51, 'hardstyle'), (52, 'heavy-metal'), (53, 'hip-hop'),
+(54, 'holidays'), (55, 'honky-tonk'), (56, 'house'), (57, 'idm'),
+(58, 'indian'), (59, 'indie'), (60, 'indie-pop'), (70, 'industrial'), 
+(71, 'iranian'), (72, 'j-dance'), (73, 'j-idol'), (74, 'j-pop'), (75, 'j-rock'),
+(76, 'jazz'), (77, 'k-pop'), (78, 'kids'), (79, 'latin'), (80, 'latino'),
+(81, 'malay'), (82, 'mandopop'), (83, 'metal'), (84, 'metal-misc'), 
+(85, 'metalcore'), (86, 'minimal-techno'), (87, 'movies'), (88, 'mhb'),
+(89, 'new-age'), (90, 'new-release'), (91, 'opera'), (92, 'pagode'), (93, 'party'),
+(94, 'philippines-opm'), (95, 'piano'), (96, 'pop'), (97, 'pop-film'),
+(98, 'post-dubstep'), (99, 'power-pop'), (100, 'progressive-house'),
+(101, 'psych-rock'), (102, 'punk'), (103, 'punk-rock'),(104, 'r-n-b'),
+(105, 'rainy-day'),(106, 'reggae'),(107, 'reggaeton'),(108, 'road-trip'),
+(109, 'rock'),(110, 'rock-n-roll'),(111, 'rockabilly'),(112, 'romance'),
+(113, 'sad'),(114, 'salsa'),(115, 'samba'),(116, 'sertanejo'),(117, 'show-tunes'),
+(118, 'singer-songwriter'),(119, 'ska'),(120, 'sleep'),(121, 'songwriter'),
+(122, 'soul'),(123, 'soundtracks'),(124, 'spanish'),(125, 'study'),(126, 'summer'),
+(127, 'swedish'),(128, 'synth-pop'),(129, 'tango'),(130, 'techno'),(131, 'trance'),
+(132, 'trip-hop'),(133, 'turkish'),(134, 'work-out'),(135, 'world-music')
+
+INSERT INTO connect (gid, mid)
+VALUES (20, 1), (1,2)
